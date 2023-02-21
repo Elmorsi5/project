a673a3746e14b521d93da6b622a24124f9d78e14
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-02-21 20:56:58 +0200

    finishing product model with images upload edits

--- products/models.py
+++ products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     content = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to="photo%y%m%d")
+    image = models.ImageField(upload_to='photos/%y/%m/%d')
     active = models.BooleanField(default=True)
