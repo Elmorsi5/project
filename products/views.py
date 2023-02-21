3cc25e88b89916707240175eee9ba1670b0cfd05
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-02-21 21:12:53 +0200

    create products views and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def product(request):
+    return render(request, "products/product.html")
+
+
+def products(request):
+    return render(request, "products/products.html")
